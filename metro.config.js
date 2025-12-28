7e1d0f1c6451be3c63bc524e444575aae384ddd0
ben <ben@shrews.io>
2025-12-28 16:08:52 -0700

    Add web shim for react-native-screens

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,19 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
 config.resolver.sourceExts = ['web.js', 'web.jsx', 'web.ts', 'web.tsx', ...config.resolver.sourceExts];
+
+// Alias react-native-screens to our web shim
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (platform === 'web' && moduleName === 'react-native-screens') {
+    return {
+      filePath: path.resolve(__dirname, 'react-native-screens.web.js'),
+      type: 'sourceFile',
+    };
+  }
+  return context.resolveRequest(context, moduleName, platform);
+};
 
 module.exports = config;
---
+++ react-native-screens.web.js
@@ -0,0 +1,27 @@
+// Web shim for react-native-screens
+import { View } from 'react-native';
+
+export const enableScreens = () => {};
+export const enableFreeze = () => {};
+export const screensEnabled = () => true;
+
+export const Screen = View;
+export const ScreenContainer = View;
+export const ScreenStack = View;
+export const ScreenStackHeaderConfig = View;
+export const ScreenStackHeaderSubview = View;
+export const ScreenStackHeaderCenterView = View;
+export const ScreenStackHeaderRightView = View;
+export const ScreenStackHeaderLeftView = View;
+export const ScreenStackHeaderSearchBarView = View;
+export const SearchBar = View;
+export const FullWindowOverlay = View;
+
+export default {
+  enableScreens,
+  enableFreeze,
+  screensEnabled,
+  Screen,
+  ScreenContainer,
+  ScreenStack,
+};
